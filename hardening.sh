f3f5bb411c06e79f273c92760273a823024887a7
Haris Qazi <48071052+harisqazi1@users.noreply.github.com>
2023-06-15 20:28:44 -0500

    Update hardening.sh

--- hardening.sh
+++ hardening.sh
@@ -1,2 +1,7 @@
 # Script for harding Ubuntu-based OS
 # based on https://privsec.dev/posts/linux/desktop-linux-hardening/#microcode-updates
+
+
+# NetworkManager Trackability Reduction
+# 
+echo "[device]\nwifi.scan-rand-mac-address=yes\n\nwifi.cloned-mac-address=random\nethernet.cloned-mac-address=random" > /etc/NetworkManager/conf.d/00-macrandomize.conf
